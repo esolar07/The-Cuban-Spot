16f0acdd8e56865d76e6bf1606e81c05484a931f
esolar07 <esolar07@gmail.com>
2017-01-28 23:21:12 -0500

    started converting site to php

---
+++ admin.php
@@ -0,0 +1,21 @@
+<?php
+    
+    <form>
+        <div class="form-group">
+            <label for="nameOfLocation">Name of Location</label>
+            <input type="text" class="form-control" id="nameOfLocation" placeholder="">
+        </div>
+        <div class="form-group">
+            <label for="address">Address</label>
+            <input type="text" class="form-control" id="address" placeholder="">
+        </div>
+        <div class="form-group">
+            <label for="startTime">Time</label>
+            <input type="text" class="form-control" id="startTime" placeholder="">
+        </div>
+        <div class="form-group">
+            <label for="endTime">Time</label>
+            <input type="text" class="form-control" id="endTime" placeholder="">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+    </form>
---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="zxx">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Food Truck</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link href="css/font-awesome.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/swiper.css" rel="stylesheet">
+    <link href="css/extralayers.css" rel="stylesheet">
+    <link href="css/settings.css" rel="stylesheet">
+    <link href="css/animate.min.css" rel="stylesheet">
+    <link href="css/prettyPhoto.css" rel="stylesheet">
+    <link href="css/demo.css" rel="stylesheet">
+    <link href="css/jquery.mmenu.all.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+      <!-- Page pre loader -->
+      <div id="pre-loader">
+          <div class="loader-holder">
+              <div class="frame">
+                  <img src="images/Preloader.gif" alt="Food Truck"/>
+              </div>
+          </div>
+      </div>
